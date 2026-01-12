c9c9412b557408a78ed2e15a3755d10413a241ce
Ruddy <baviere.ruddy62@gmail.com>
2026-01-12 16:59:21 +0100

    :sparkles: feat: update megamenu

--- galanet/src/components/navbar/MainNav.tsx
+++ galanet/src/components/navbar/MainNav.tsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router"
 import { links } from "../../data/link"
+import { useState } from 'react';
+import { MegaMenu } from "./MegaMenu";
 
 export const MainNav = () => {
-
+    const [isHovering, setIsHovering] = useState(false);
     return (
+        <>
         <nav className="overflow-x-auto xl:overflow-x-visible scrollbar-hide">
             <ul className='flex items-center whitespace-nowrap gap-3 py-4 xl:gap-0 xl:py-0 xl:justify-between'>
                 <div className="xl:hidden">
@@ -10,14 +14,18 @@ export const MainNav = () => {
                 </div>
                 {
                     links.map((link, index) => (
+                        <div  onMouseEnter={() => setIsHovering(true)} onMouseLeave={()=>setIsHovering(false)}>
                         <li key={index}>
-                            <a href={link.href} className="text-slate-50 p-3 border border-slate-400 rounded-full xl:p-0 xl:border-0 xl:rounded-none">
+                            <Link to={link.href} className={` text-slate-50 p-3 border border-slate-400 rounded-full xl:p-0 xl:border-0 xl:rounded-none`}>
                                 {link.text}
-                            </a>
+                            </Link>
                         </li>
+                        </div>
                     ))
                 }
             </ul>
+                {isHovering && <MegaMenu/>}
         </nav>
+        </>
     )
 }
---
+++ galanet/src/components/navbar/MegaMenu.tsx
@@ -0,0 +1,11 @@
+import { links } from "../../data/link"
+export const MegaMenu = () => {
+    return(
+        <div className="bg-slate-900 w-full h-full absolute z-99">
+            <div className="bg-slate-700 w-1/6">
+                <p>yjvjgvngcbcgcxg</p>
+            </div>
+            <p>vhfhchgvghchgcghchg</p>
+        </div>
+    )
+}
